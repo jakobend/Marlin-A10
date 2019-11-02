627e6b95cd76fc4329134adaec67512950e0edd5
jakobend <jakobend@users.noreply.github.com>
2019-11-02 16:08:36 +0100

    Readjust travel limits to sane values (5mm safety distance)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -886,8 +886,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 240
-#define Y_BED_SIZE 240
+#define X_BED_SIZE 235
+#define Y_BED_SIZE 230
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -895,7 +895,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 260
+#define Z_MAX_POS 250
 
 /**
  * Software Endstops
